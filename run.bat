3410d68d092529cc9518c054459c2cc4a6d2a644
chg-chg <1792354623@qq.com>
2022-07-21 21:41:30 +0800

    yeah

--- run.bat
+++ run.bat
@@ -1,15 +1,4 @@
-@REM rem Copyright 2022 ch'g
-@REM rem 
-@REM rem Licensed under the Apache License, Version 2.0 (the "License");
-@REM rem you may not use this file except in compliance with the License.
-@REM rem You may obtain a copy of the License at
-@REM rem 
-@REM rem     http://www.apache.org/licenses/LICENSE-2.0
-@REM rem 
-@REM rem Unless required by applicable law or agreed to in writing, software
-@REM rem distributed under the License is distributed on an "AS IS" BASIS,
-@REM rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@REM rem See the License for the specific language governing permissions and
-@REM rem limitations under the License.
-
-python3 jemdoc index.jemdoc
+python jemdoc index.jemdoc
+python jemdoc link.jemdoc 
+python jemdoc mathjax.jemdoc 
+python jemdoc underscore.jemdoc  
