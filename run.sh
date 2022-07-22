49ce764d64ebdb465602c8cf913df2843cb787a3
chg-chg <1792354623@qq.com>
2022-07-22 17:15:22 +0800

    try

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 python3 jemdoc.py index.jemdoc
-python3 jemdoc.py link.jemdoc 
-python3 jemdoc.py mathjax.jemdoc 
+python3 jemdoc.py link.jemdoc  
+python3 jemdoc.py -c mysite.conf mathjax.jemdoc 
+python3 jemdoc.py -c mysite.conf jem.jemdoc 
 python3 jemdoc.py underscore.jemdoc  
