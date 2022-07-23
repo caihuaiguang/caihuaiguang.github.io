c3ded6d40f5e349a5c78b44aad85346938c4b452
chg-chg <1792354623@qq.com>
2022-07-23 10:07:18 +0800

    new pages

--- run.sh
+++ run.sh
@@ -3,3 +3,19 @@ python3 jemdoc.py link.jemdoc
 python3 jemdoc.py -c mysite.conf mathjax.jemdoc 
 python3 jemdoc.py -c mysite.conf jem.jemdoc 
 python3 jemdoc.py underscore.jemdoc  
+cd www
+python3 ../jemdoc.py index.jemdoc  
+python3 ../jemdoc.py download.jemdoc  
+python3 ../jemdoc.py revision.jemdoc  
+python3 ../jemdoc.py contact.jemdoc  
+python3 ../jemdoc.py using.jemdoc  
+python3 ../jemdoc.py example.jemdoc  
+python3 ../jemdoc.py cheatsheet.jemdoc  
+python3 ../jemdoc.py extra.jemdoc  
+python3 ../jemdoc.py htmlchanges.jemdoc  
+python3 ../jemdoc.py menu.jemdoc  
+python3 ../jemdoc.py modelines.jemdoc  
+python3 ../jemdoc.py stuff.jemdoc  
+python3 ../jemdoc.py latex.jemdoc  
+python3 ../jemdoc.py tables.jemdoc   
+python3 ../jemdoc.py -c ../mysite.conf latex.jemdoc   
