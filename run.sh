2936652cf4ba68a3425fb9e3743b0a38414c3fae
chg-chg <1792354623@qq.com>
2022-07-23 16:30:28 +0800

    1

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ python3 jemdoc.py link.jemdoc
 python3 jemdoc.py -c mysite.conf mathjax.jemdoc 
 python3 jemdoc.py -c mysite.conf jem.jemdoc  
 cd www
-python3 ../jemdoc.py www.jemdoc  
+python3 ../jemdoc.py index.jemdoc  
 python3 ../jemdoc.py download.jemdoc  
 python3 ../jemdoc.py revision.jemdoc  
 python3 ../jemdoc.py contact.jemdoc  
